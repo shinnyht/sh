7ea4770f1ccfc0b441a45b9774d7827260a3db2a
Shinsuke Takagi <shinny@ht.sfc.keio.ac.jp>
2016-02-26 03:58:34 +0900

    update directory

--- scripts/rakutenautosearch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-REPOSITORY_PATH='/Users/shinny/git/Others/RakutenAutoSearch/'
-SCRIPT_PATH='src/execute.js'
-LOG_PATH='log/'
-DATE=`date '+%F'`
-
-/usr/local/bin/node $REPOSITORY_PATH$SCRIPT_PATH >> $REPOSITORY_PATH$LOG_PATH$DATE.log
